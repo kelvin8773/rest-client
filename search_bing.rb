816a8144b0aa6c26c17b15d44a2f90dbc2811d45
Kelvin Liang <kelvin9877@gmail.com>
2019-06-28 23:37:17 +0800

    make better output

--- search_bing.rb
+++ search_bing.rb
@@ -1,15 +1,24 @@
 require 'rest-client'
-
 URL = 'https://www.bing.com/search'
-p "please input the keyword for search: "
+matcher = /<h2><a.*?href="(?<link>.*?)".*?h=.*?>(?<text>.*?)<\/a.*?>/
+
+p "please input the keyword for search:"
 input = gets.chomp
 
 response = RestClient.get URL, {params: {q: input}}
 
-p response.code
-p response.cookies
-p response.headers
+# p response.code
+# p response.cookies
+# p response.headers
+
+p "Here is the result:"
+p "##################################"
 
 
-result = response.body.scan(/<h2><a.*?href="(?<link>.*?)".*?h=.*?>(?<text>.*?)<\/a.*?>/)
-p result
+result = response.body.scan(matcher)
+
+result.each do |x|
+  p "#{x[1]}"
+  p "#{x[0]}"
+  p "-------------------------------------------------------------"
+end
